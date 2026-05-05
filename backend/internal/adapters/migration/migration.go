2a5c21f77138d9a2dcd14b1bc1b7b79b6dc948af
aditokmo <adit.github@gmail.com>
2026-05-05 22:51:16 +0200

    updating driver

--- backend/internal/adapters/migration/migration.go
+++ backend/internal/adapters/migration/migration.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
-	_ "github.com/golang-migrate/migrate/v4/database/pgx"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
